dd455fce31c70742af015e5906aa282d83da0fe6
Rafael Sanchez <88865750+RafaellSanchez@users.noreply.github.com>
2023-12-16 16:24:43 +0000

    upgrade testing

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print('Testando código de agendamento!')
+
+
+file = '/workspaces/testing-scheduler/data_testing.txt'
+
+with open(file, 'r') as arquivo:
+    linhas = arquivo.readlines()
+    for linha in linhas:
+        dados = linha.split(';')  # Divide a linha usando o ponto e vírgula como separador
+        print(dados)  # Isso imprimirá uma lista com os dados separados
+
+print("Teste sucesso!")
