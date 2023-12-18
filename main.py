7b2f16e7b05d94d142a4bbc2e63a478a2da6fbb2
Rafael Sanchez <88865750+RafaellSanchez@users.noreply.github.com>
2023-12-18 02:34:37 +0000

    upgrade testing main.py

--- main.py
+++ main.py
@@ -13,15 +13,35 @@ print("Conteúdo do diretório:", os.listdir())
 full_path = os.path.abspath(file)
 print("Caminho completo do arquivo:", full_path)
 
-# Tente abrir o arquivo
+conteudo = ''  # Inicializa uma string vazia para armazenar o conteúdo do arquivo
+
+# Tente abrir o arquivo e ler seu conteúdo
 try:
     with open(full_path, 'r') as arquivo:
         linhas = arquivo.readlines()
         for linha in linhas:
             dados = linha.split(';')
             print(dados)
-    print("Teste sucesso!")
+            conteudo += linha  # Adiciona a linha ao conteúdo a ser salvo
+    print("Leitura do arquivo bem-sucedida!")
 except FileNotFoundError:
     print("Arquivo não encontrado:", full_path)
 except Exception as e:
     print("Erro:", e)
+
+# Diretório de destino para salvar o arquivo
+diretorio_destino = 'dados'  # Substitua pelo nome do seu diretório de destino
+
+# Nome do arquivo a ser criado no diretório de destino
+nome_arquivo_destino = 'arquivo_salvo.txt'
+
+# Caminho completo para o arquivo de destino
+caminho_arquivo_destino = os.path.join(diretorio_destino, nome_arquivo_destino)
+
+# Tenta salvar o conteúdo lido do arquivo no diretório de destino
+try:
+    with open(caminho_arquivo_destino, 'w') as arquivo_destino:
+        arquivo_destino.write(conteudo)
+    print(f"Conteúdo do arquivo salvo em '{caminho_arquivo_destino}'.")
+except Exception as e:
+    print("Erro ao salvar o arquivo:", e)
